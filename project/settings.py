ea6c74f5888dd2a22d187825db1a1fcce137dcd1
BALLA LAKSHMI KRISHNA VAMSI <131175410+KrishnaVamsi4229@users.noreply.github.com>
2024-11-30 11:13:15 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*(xm56#gtn&-z65^c+qk-(t!2nj*o0)esg0#5t+@mydu@(bg)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 AUTH_USER_MODEL = 'app.User'
 
